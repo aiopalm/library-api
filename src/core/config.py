4b9f1d984eabf12d688392d37d119922cfbc28a7
Palm <palmware66@gmail.com>
2025-12-11 23:11:55 +0300

    initial commit

--- src/core/config.py
+++ src/core/config.py
@@ -1,7 +1,7 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
-    model_config = SettingsConfigDict(env_file=".env", env_ignore_empty=True)
+    model_config = SettingsConfigDict(env_file=".env", env_ignore_empty=True, extra='ignore')
 
     SECRET_KEY: str = "test-secret-key"
     ALGORITHM: str = "HS256"
--- src/main.py
+++ src/main.py
@@ -1,6 +1,13 @@
-def main():
-    ...
+from fastapi import FastAPI
 
+# from src.api import auth, books, readers, borrowing
 
-if __name__ == '__main__':
-    main()
+app = FastAPI(
+    title="Library API",
+    description="API для управления библиотекой"
+)
+
+# app.include_router(auth.router, prefix="/auth", tags=["Authentication"])
+# app.include_router(books.router, prefix="/books", tags=["Books"])
+# app.include_router(readers.router, prefix="/readers", tags=["Readers"])
+# app.include_router(borrowing.router, prefix="/borrow", tags=["Borrowing"])
