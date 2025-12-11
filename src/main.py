513cdd7f996cb08c8a5e70ae8a70c1650245f288
Palm <palmware66@gmail.com>
2025-12-12 01:14:51 +0300

    connected auth router

--- src/main.py
+++ src/main.py
@@ -1,13 +1,10 @@
 from fastapi import FastAPI
 
-# from src.api import auth, books, readers, borrowing
+from src.api import auth
 
 app = FastAPI(
     title="Library API",
-    description="API для управления библиотекой"
+    description="API for library management"
 )
 
-# app.include_router(auth.router, prefix="/auth", tags=["Authentication"])
-# app.include_router(books.router, prefix="/books", tags=["Books"])
-# app.include_router(readers.router, prefix="/readers", tags=["Readers"])
-# app.include_router(borrowing.router, prefix="/borrow", tags=["Borrowing"])
+app.include_router(auth.router, prefix="/auth", tags=["Authentication"])
