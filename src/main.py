74f3331ddf56d203e05fa7b8fd87b4cec797e9c1
Palm <palmware66@gmail.com>
2025-12-14 15:18:37 +0300

    connected borrowing router

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 
-from src.api import auth, books, readers
+from src.api import auth, books, readers, borrowing
 
 app = FastAPI(
     title="Library API",
@@ -10,3 +10,4 @@ app = FastAPI(
 app.include_router(auth.router, prefix="/auth", tags=["Authentication"])
 app.include_router(books.router, prefix="/books", tags=["Books"])
 app.include_router(readers.router, prefix="/readers", tags=["Readers"])
+app.include_router(borrowing.router, prefix="/borrowing", tags=["Borrowing"])
